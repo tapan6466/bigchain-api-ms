1979825dba1614cb5afc33b6d1c5de08e7dea14b
Lokesh <lokesh3440@gmail.com>
2017-04-14 22:50:29 +0800

    files updated

--- src/main/java/com/pwc/bigchain/app/BigchainApiMsApplication.java
+++ src/main/java/com/pwc/bigchain/app/BigchainApiMsApplication.java
@@ -2,8 +2,10 @@ package com.pwc.bigchain.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BigchainApiMsApplication {
 
 	public static void main(String[] args) {
